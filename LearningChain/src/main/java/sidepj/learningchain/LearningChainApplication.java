2185b8a00b0e4b3984a6ffbb44cfee3948b88827
HOSE JOO <hose0728@naver.com>
2022-06-12 19:50:23 +0900

    :rocket: init Springboot PJ
    init Springboot PJ

---
+++ LearningChain/src/main/java/sidepj/learningchain/LearningChainApplication.java
@@ -0,0 +1,13 @@
+package sidepj.learningchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningChainApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LearningChainApplication.class, args);
+  }
+
+}
---
+++ LearningChain/src/test/java/sidepj/learningchain/LearningChainApplicationTests.java
@@ -0,0 +1,13 @@
+package sidepj.learningchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningChainApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
